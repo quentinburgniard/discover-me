b5b782384c683f93c11b9fc798203d25131e5bda
Quentin Burgniard <quentinburgniard@gmail.com>
2019-02-27 10:23:02 +0100

    feat(service): github and strava integration

---
+++ src/app/github.ts
@@ -0,0 +1,3 @@
+export class GitHub {
+    repos: number
+}
--- src/app/strava.service.ts
+++ src/app/strava.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { Observable } from 'rxjs';
-import { catchError, tap } from 'rxjs/operators';
 
 import { Strava } from './strava';
 
